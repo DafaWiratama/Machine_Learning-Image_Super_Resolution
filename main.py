1d35b46d6a73bf926e6647530776d9bb3c143145
dafaw <dafawiratama13@gmail.com>
2022-05-07 17:51:53 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from res import models, io
+from flask import Flask, request,  Response
+from waitress import serve
+
+model = models.ESRGAN("models/esrgan.tf")
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upscaler():
+    if request.method == 'POST':
+        file = request.files['file']
+        file_bytes = file.read()
+        image = io.load_image_from_bytes(file_bytes)
+        output = model(image)
+        image_bytes = io.encode_png(output)
+        return Response(image_bytes, mimetype='image/png')
+    return """
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <input type="submit">
+    </form>
+    """
+
+
+serve(app, host='0.0.0.0', port=10_002)
